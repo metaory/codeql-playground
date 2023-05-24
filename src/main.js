433a2dbe5457b51438a0ec083ab8eae2696b5708
Pou Yan <metaory@gmail.com>
2023-05-24 15:22:39 +0800

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,13 @@
+function Graph(nodes, edges) {
+  this.nodes = nodes;
+  this.edges = edges;
+  // cache minimum distance between pairs of nodes
+  this.distance = {};
+}
+elem.parentNode.selectedIndex;
+function Rectangle(x, y, width, height) {
+	this.x = x;
+	this.y = y;
+	width = width;
+	this.height = height;
+}
