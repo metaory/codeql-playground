b5bacd023efc215f959a7ced729c024f2a898c2b
metaory <metaory@gmail.com>
2023-05-25 10:48:17 +0800

    bad code

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
 function Graph(nodes, edges) {
   this.nodes = nodes;
   this.edges = edges;
-  // cache minimum distance between pairs of nodes
+  // Cache minimum distance between pairs of nodes
   this.distance = {};
 }
+
 elem.parentNode.selectedIndex;
 function Rectangle(x, y, width, height) {
-	this.x = x;
-	this.y = y;
-	width = width;
-	this.height = height;
+  this.x = x;
+  this.y = y;
+  width = width;
+  this.height = height;
 }
+
+function isOdd(x) {
+  return x % 2 === 1;
+}
+
+console.log(isOdd(-9)); // Prints 'false'
