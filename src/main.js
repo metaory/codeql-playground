976cfe51f446e79e2b1dfc1e09dcb553ed2594a6
metaory <metaory@gmail.com>
2023-05-25 10:51:22 +0800

    bad code

--- src/main.js
+++ src/main.js
@@ -20,3 +20,35 @@ function isOdd(x) {
 console.log(isOdd(-9)); // Prints 'false'
 
 elem.parentNode.selectedIndex;
+
+async function getData(id) {
+  const req = await fetch(`https://example.com/data?id=${id}`);
+  if (!req.ok) {
+    return null;
+  }
+
+  return req.json();
+}
+
+async function showData(id) {
+  const data = getData(id);
+  if (data == null) {
+    console.warn("No data for: " + id);
+  }
+  // ...
+}
+
+function error(msg) {
+  console.log(msg);
+}
+
+function processResponse(response) {
+  if (response.status === 200) {
+    var error = processResponseText(response.responseText);
+    if (error) {
+      throw error;
+    }
+  } else {
+    error("Unexpected response status " + response.status);
+  }
+}
